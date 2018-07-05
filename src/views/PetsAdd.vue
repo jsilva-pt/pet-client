caa61c69ae5b2a4e37e46c2e734564791778f3a6
jmanuelsilva <jmanuelsilva.pt@gmail.com>
2018-07-05 22:57:13 +0100

    Added form to add a pet

--- src/views/PetsAdd.vue
+++ src/views/PetsAdd.vue
@@ -1,12 +1,96 @@
 <template>
   <div class="pets-add">
-    Form to add a pet to adoption
+
+    <v-btn
+      flat
+      icon 
+      color="primary"
+      @click="$router.go(-1)">
+      <v-icon>arrow_back</v-icon>
+    </v-btn>
+
+    <v-form ref="form">
+      <v-text-field
+        label="Name"
+        v-model="name"
+      ></v-text-field>
+
+      <v-radio-group v-model="gender">
+        <v-radio
+          v-for="gender in genders"
+          :key="gender.code"
+          :label="gender.label"
+          :value="gender.code"
+        ></v-radio>
+      </v-radio-group>
+
+      <v-checkbox
+        hide-details
+        v-for="item in colors"
+        v-model="color"
+        :key="item.code"
+        :label="item.label"
+        :value="item.code"
+        class="mt-0"
+      ></v-checkbox>
+
+      <v-btn
+        color="primary"
+        @click="submit()">
+        Add
+      </v-btn>
+    </v-form>
+
   </div>
 </template>
 
 <script>
+import PET_CREATE from '../graphql/PetCreate.gql'
 export default {
-  name: 'pets-add'
+  name: 'pets-add',
+  data: () => ({
+    name: null,
+    gender: 'male',
+    color: [],
+    genders: [
+      {
+        label: 'Male',
+        code: 'male'
+      }, {
+        label: 'Female',
+        code: 'female'
+      }
+    ],
+    colors: [
+      {
+        code: 'white',
+        label: 'White'
+      },
+      {
+        code: 'black',
+        label: 'Black'
+      }
+    ]
+  }),
+  methods: {
+    submit () {
+      const name = this.name
+      const gender = this.gender
+      const color = this.color
+
+      this.$apollo.mutate({
+        mutation: PET_CREATE,
+        variables: {
+          name,
+          gender,
+          color
+        },
+        update: (store, { data: { newTag } }) => {
+          this.$router.go(-1)
+        }
+      })
+    }
+  }
 }
 </script>
 
